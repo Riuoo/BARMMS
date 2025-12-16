ae588dc8a298e4d5b6fb4707fab2c6ef7267bfe7
Riuoo <rodericktajo02@gmail.com>
2025-12-16 23:04:11 +0800

    make the favicon work when host

--- resources/views/admin/main/layout.blade.php
+++ resources/views/admin/main/layout.blade.php
@@ -62,7 +62,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
-    <link rel="icon" href="{{ asset('lower malinao logo.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     @vite(['resources/css/app.css'])
     @notifyCss
     <!-- Tom Select for searchable dropdowns -->
--- resources/views/login/landing.blade.php
+++ resources/views/login/landing.blade.php
@@ -6,7 +6,7 @@
     <title>Lower Malinao System</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
-    <link rel="icon" href="{{ asset('lower malinao logo.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     @notifyCss
     <style>
         .hero-bg {
